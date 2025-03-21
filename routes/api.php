131ce2cd6290299098dd6250bf216c8d92e58f22
pedroalano <pedroalano@gmail.com>
2025-03-21 11:44:36 -0300

    Feat: Add API KEY Authorization

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/generate-api-key', [AuthController::class, 'generateApiKey']);
 
-Route::get('/', [ProductController::class, 'index']);
-Route::put('/products/{code}', [ProductController::class, 'update']);
-Route::delete('/products/{code}', [ProductController::class, 'delete']);
-Route::get('/products/{code}', [ProductController::class, 'show']);
-Route::get('/products', [ProductController::class, 'list']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::put('/products/{code}', [ProductController::class, 'update']);
+    Route::delete('/products/{code}', [ProductController::class, 'delete']);
+    Route::get('/products/{code}', [ProductController::class, 'show']);
+    Route::get('/products', [ProductController::class, 'list']);
+});
